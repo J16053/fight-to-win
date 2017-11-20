f1108aef516f0d789ba416a5f4505b199a918771
Jacinta <jh5372@nyu.edu>
2017-11-20 21:49:11 +0400

    .DS_Store banished! BattleshipBot.py made more succinct and legible!

--- BattleshipBot.py
+++ BattleshipBot.py
@@ -1,34 +1,19 @@
 import network
 import random,time,os
 
-neighbours = [[0,-1],[0,1],[-1,0],[1,0]]
-neighbourhood = [[0,0],[0,-1],[-1,0],[0,1],[1,0],[0,-2],[-2,0],[0,2],[2,0],[0,-3],[-3,0],[0,3],[3,0]]
-
-hint = []
-hit = []
-guesses = []
-shipSize = 4
-
-hinted = [[i,j] for i in range(-4,5) for j in range(-4,5) if not (abs(i) > 1 and abs(j) > 1)]
-
 def update(location): #updates probability of everything near guess
 	global board
-	row = location[0]
-	col = location[1]
 	for n in neighbourhood:
-		r = row+n[0]
-		c = col+n[1]
-		pos = [r,c]
-		if status(pos) != -1:
-			board[r][c] = probability(pos)
+		row = location[0]+n[0]
+		col = location[1]+n[1]
+		if status([row,col]) != -1:
+			board[row][col] = probability([row,col])
 
 def clue(location, area): #appends all locations outside of area to guesses
 	global board
-	row = location[0]
-	col = location[1]
 	valid = []
-	for n in area:	#everything in 3*4 area on either side:
-		pos = [row+n[0],col+n[1]]
+	for n in area:
+		pos = [location[0]+n[0],location[1]+n[1]]
 		if status(pos) != -1:
 			valid.append(pos)
 	for i in range(size):
@@ -38,16 +23,11 @@ def clue(location, area): #appends all locations outside of area to guesses
 	board = pdensity()
 
 def randomGuess(): #returns guess based on highest probability
-	mostLikely = []
-	for v in range(8,0,-1):
+	for v in range(8,0,-1): #v for value
 		for i in range(size):
 			for j in range(size):
 				if board[i][j] == v and [i,j] not in hit:
-					s = str(i)+','+str(j)
-					mostLikely.append(s)
-		if len(mostLikely) > 0:
-			break
-	return mostLikely[0]
+					return str(i)+','+str(j)
 
 def pdensity(): #populates board with probabilities
 	board = []
@@ -74,39 +54,40 @@ def probability(location): #returns number of ship orientations possible at loca
 	col = location[1]
 	vertical = 0
 	horizontal = 0
-	for n in neighbours[:2]:
+	for n in neighbours:
 		for i in range(1,shipSize):
 			pos = [row+i*n[0],col+i*n[1]]
 			if status(pos) == 0:
-				horizontal += 1
+				if pos[0] == row: #if pos is to the left or right of location
+					horizontal += 1
+				else: #if pos is above or below location
+					vertical += 1
 			else:
 				break
-	for n in neighbours[2:]:
-		for i in range(1,shipSize):
-			pos = [row+i*n[0],col+i*n[1]]
-			if status(pos) == 0:
-				vertical += 1
-			else:
-				break
-	if vertical < (shipSize-2) and horizontal < (shipSize-2):
-		p = 0
-	elif vertical < (shipSize-2):
-		p = horizontal-(shipSize-2)
-	elif horizontal < (shipSize-2):
-		p = vertical-(shipSize-2)
-	else:
-		p = vertical+horizontal-2*(shipSize-2)
+	overlap = shipSize-2
+	if vertical < overlap:
+		vertical = overlap
+	if horizontal < overlap:
+		horizontal = overlap
+	p = vertical + horizontal - 2*overlap
 	return p
 
-name = 'Jacinta'	#input ("Please enter your name")
+name = 'Jacinta'
+#name = input ("Please enter your name")
 
 # create boards
 dispBoard, size, shipSize, mines = network.createBoards(name, True)
-# hidboard, dispBoard, size, shipSize, mines = network.createBoards(name)
+#hidboard, dispBoard, size, shipSize, mines = network.createBoards(name)
+
+
+neighbours = [[0,-1],[0,1],[-1,0],[1,0]] #left, right, up, down
+neighbourhood = [[n[0]*i,n[1]*i] for n in neighbours for i in range(1,shipSize)]
+neighbourhood.append([0,0])
+hinted = [[i,j] for i in range(-4,5) for j in range(-4,5) if not (abs(i) > 1 and abs(j) > 1)]
 
 board = pdensity()
-
-moves = 0
+hit = [] #locations of opponent's ship
+guesses = [] #locations that definitely do not contain ship
 
 while True:
 	################################################################################
@@ -123,9 +104,7 @@ while True:
 	elif ans != None:
 		ans = list(map(int,ans.split(',')))
 		print (ans[0],ans[1], "is nearby location of the ship")
-		if len(hit) == 0:
-			clue([ans[0],ans[1]], hinted)
-		hint.append([ans[0],ans[1]])
+		clue([ans[0],ans[1]], hinted)
 
 	################################################################################
 	# Your turn to hit
@@ -138,6 +117,7 @@ while True:
 		guess = randomGuess()
 	else:
 		guess = randomGuess()
+	#guess = input("Enter your guess: ")
 	
 	ans = network.send(guess)
 	location = [int(guess[0]),int(guess[-1])]
